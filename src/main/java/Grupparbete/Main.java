cbae36e9a53b6dd127524c04f88628491a18538e
zeldoz <johannes.mounzer@hotmail.com>
2024-10-31 10:33:43 +0100

    tja Johannes här.

--- src/main/java/Grupparbete/Main.java
+++ src/main/java/Grupparbete/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Gruppen!");
         System.out.println("hej de Athir");
+        System.out.println("Nej det e jag som e Athir");
     }
 }
