f13bafcd756ba6099df19890266368d8c21abea5
swestlund <simonrtwestlund@gmail.com>
2024-10-31 10:38:32 +0100

    add sout test

--- src/main/java/Grupparbete/Main.java
+++ src/main/java/Grupparbete/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello Gruppen!");
         System.out.println("hej de Athir");
         System.out.println("Nej det e jag som e Athir");
+        System.out.println("000000000000010101011000101010011");
     }
 }
