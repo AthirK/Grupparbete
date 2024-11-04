97c2a4bc56cf5d77cc77e8276c97b69b18b2c179
AthirK <athirkazem@gmail.com>
2024-11-04 12:55:25 +0100

    övning 1

--- src/main/java/Grupparbete/Main.java
+++ src/main/java/Grupparbete/Main.java
@@ -1,9 +1,23 @@
 package Grupparbete;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello Gruppen!");
-        System.out.println("hej de Athir");
-        System.out.println("Nej det e jag som e Athir");
+    public static void main(String[] args)
+    {
+
+        List<User> users = Arrays.asList(
+                new User("Kalle", "Kalle@gmail.com", 12, UserRole.ADMIN, true),
+                new User("Karin", "Karin@gmail.com", 4, UserRole.EMPLOYEE, true),
+                new User("Petter", "Petter@gmail.com", 2, UserRole.ADMIN, true),
+                new User("Sven-Ingvar", "Sven-Ingvar@gmail.com", 5, UserRole.EMPLOYEE, true)
+                );
+
+        UserService userService = new UserService();
+        List<UserDTO> result = userService.getActiveAdminUsers(users);
+
+        System.out.println("Filtered Active Admin Users: " + result);
     }
 }
