621db9adc8bb7bb1b17ae18a1c6c9d7103f3dec5
AthirK <athirkazem@gmail.com>
2024-11-04 14:52:24 +0100

    övning 3

---
+++ src/main/java/Grupparbete/Calculator.java
@@ -0,0 +1,16 @@
+package Grupparbete;
+
+public class Calculator
+{
+    private int number;
+
+    private Calculator(int number)
+    {
+        this.number = number;
+    }
+
+    private int add(int other)
+    {
+        return this.number + other;
+    }
+}
--- src/main/java/Grupparbete/Main.java
+++ src/main/java/Grupparbete/Main.java
@@ -1,13 +1,20 @@
 package Grupparbete;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 
+
+
 public class Main {
+    private static Object Calculator;
+
     public static void main(String[] args)
     {
         //övning 1--------------------------------------------
-        List<User> users = Arrays.asList(
+        /*List<User> users = Arrays.asList(
                 new User("Kalle", "Kalle@gmail.com", 12, UserRole.ADMIN, true),
                 new User("Karin", "Karin@gmail.com", 4, UserRole.EMPLOYEE, true),
                 new User("Petter", "Petter@gmail.com", 2, UserRole.ADMIN, true),
@@ -15,9 +22,29 @@ public class Main {
                 );
 
         UserService userService = new UserService();
+
         List<UserDTO> result = userService.getActiveAdminUsers(users);
 
-        System.out.println("Filtered Active Admin Users: " + result);
-        //----------------------------------------------------
+        System.out.println("Filtered Active Admin Users: " + result);*/
+        //övning 3---------------------------------------------
+        try
+        {
+            Class<Calculator> Clazz = Calculator.class;
+
+            Constructor<Calculator> constructor = Clazz.getDeclaredConstructor(int.class);
+            constructor.setAccessible(true);
+
+            Calculator calculator = constructor.newInstance(32);
+
+            Method addMethod = Clazz.getDeclaredMethod("add", int.class);
+            addMethod.setAccessible(true);
+
+            int result = (int) addMethod.invoke(calculator, 18);
+            System.out.println("Result of addition: " + result);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
     }
 }
